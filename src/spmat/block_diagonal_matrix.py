6d1de53f35845be51660a1e367e70b074adf116c
dhs2018 <dhs2018@uw.edu>
2022-09-15 13:54:48 -0700

    tests passing for both matrix classes

--- src/spmat/block_diagonal_matrix.py
+++ src/spmat/block_diagonal_matrix.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import numpy as np
 import scipy
 
@@ -41,6 +41,9 @@ class Block:
     def dot(self, other: np.array):
         return self.data.dot(other)
 
+    def inv_dot(self, other: np.array):
+        return self.inverse.dot(other)
+
     def validate(self, rtol: float = 1e-05, atol: float = 1e-08) -> None:
         """Blocks are always assumed to be square and symmetric.
 
@@ -48,6 +51,9 @@ class Block:
         might not be worth it."""
         if not np.allclose(self.data, self.data.T, rtol=rtol, atol=atol):
             raise ValueError(f"Provided data of {self.data} is not symmetric")
+
+    def __repr__(self):
+        return self.data.__repr__()
 
 
 class BDMatrix:
@@ -60,11 +66,12 @@ class BDMatrix:
         self.data = data
         self.block_sizes = block_sizes
         # Validate that shapes match
-        block_sum = np.sum(np.array(block_sizes), axis=0)
-        if (block_sum, block_sum) != data.shape:
-            raise ValueError(f"Input matrix has dimensions {data.shape}, "
-                             f"but provided blocks sum to shape {[block_sum] * 2}. "
-                             f"The shapes must exactly match.")
+        self.validate()
+
+    @property
+    def shape(self) -> Tuple[int]:
+        """Shape of the input data."""
+        return self.data.shape
 
     @property
     def blocks(self) -> List[Block]:
@@ -78,34 +85,49 @@ class BDMatrix:
                 curr_idx += nvals
         return self._blocks
 
-    def dot(self, other: np.array) -> np.array:
-        # Flatten the other array and do the dot in loops
-        # First pass: Assume other is always a 1D numpy array, organized into the same blocks.
-        result = np.array([])
-        curr_idx = 0
-        for block in self.blocks:
-            # Can we always assume "other" is a 1 or 2D array?
-            rows, _ = block.shape
-            # TODO: need to examine requirements for the "other" vector
-            dot_subset = block.dot(other[curr_idx:curr_idx + rows])
-            curr_idx += rows
-            result = np.vstack(result, dot_subset)
-        return result
-
-    def inv_dot(self, other: np.array) -> np.array:
-        """Returns the inverse of this matrix dot the input."""
-        curr_idx = 0
-        result = np.array([])
-        for block in self.blocks:
-            rows, _ = block.shape
-            invdot_subset = block.inverse.dot(other[curr_idx:curr_idx + rows])
-            result = np.vstack([result, invdot_subset])
-            curr_idx += rows
-        return result
-
     @property
     def log_determinant(self) -> float:
         """Sum of the logs."""
         if not hasattr(self, '_log_determinant'):
             self._log_determinant = np.sum([block.log_det for block in self.blocks])
         return self._log_determinant
+
+    def validate(self):
+        block_sum = np.sum(self.block_sizes)
+        if (block_sum, block_sum) != self.data.shape:
+            raise ValueError(f"Input matrix has dimensions {self.data.shape}, "
+                             f"but provided blocks sum to shape {block_sum}. "
+                             f"The shapes must exactly match.")
+
+    def dot(self, other: np.array) -> np.array:
+        """Calculate the dot product of the block diagonal array with another array."""
+        # First pass: Assume other is always a 1D numpy array, organized into the same blocks.
+
+        # Mismatched lengths will break the dot product
+        if len(other) != self.shape[0]:
+            raise ValueError(f"Provided vector has length {len(other)}, but the data has "
+                             f"shape {self.shape}. The dimensions must match.")
+
+        # Split the other array by index. np.cumsum gets the indices to split on
+        # exclude the last value to avoid returning a single empty array at the end
+        split_array = np.split(other, np.cumsum(self.block_sizes[:-1]))
+        result = np.hstack([block.dot(vector)
+                            for block, vector in zip(self.blocks, split_array)])
+        return result
+
+    def inv_dot(self, other: np.array) -> np.array:
+        """Returns the inverse of this matrix dot the input."""
+        # Mismatched lengths will break the dot product
+        if len(other) != self.shape[0]:
+            raise ValueError(f"Provided vector has length {len(other)}, but the data has "
+                             f"shape {self.shape}. The dimensions must match.")
+
+        # Split the other array by index. np.cumsum gets the indices to split on
+        # exclude the last value to avoid returning a single empty array at the end
+        split_array = np.split(other, np.cumsum(self.block_sizes[:-1]))
+        result = np.hstack([block.inv_dot(vector)
+                            for block, vector in zip(self.blocks, split_array)])
+        return result
+
+    def __repr__(self):
+        return self.data.__repr__()
--- tests/test_bdmatrix_new.py
+++ tests/test_bdmatrix_new.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pytest
+import scipy
 from spmat.block_diagonal_matrix import Block, BDMatrix
 
 
@@ -24,6 +25,34 @@ def test_block():
 
     assert np.allclose(block.inverse, np.linalg.inv(block.data))
 
+
 def test_block_matrix():
 
-    pass
+    # Create a series of blocks
+    blocks = [create_block((i, i)).data for i in range(1, 4)]
+    # Expected size: 3 blocks of 1x1, 2x2, 3x3 dimensions. End matrix is going to be 6x6
+    diagonal_mat = scipy.linalg.block_diag(*blocks)
+
+    with pytest.raises(ValueError):
+        # Create a matrix with incorrect dimensions.
+        BDMatrix(data=diagonal_mat, block_sizes=[1])
+
+    # with correct dimensions:
+    bd_matrix = BDMatrix(data=diagonal_mat, block_sizes=[1, 2, 3])
+    assert bd_matrix.shape == (6, 6)
+
+    other_mat = np.random.randn(6)
+
+    # Calculate the dot
+    # Should exactly equal the full dot product, without computing the extra 0's
+    assert np.allclose(bd_matrix.dot(other_mat), bd_matrix.data.dot(other_mat))
+
+    # Inverse dot
+    assert np.allclose(bd_matrix.inv_dot(other_mat),
+                       np.linalg.inv(bd_matrix.data).dot(other_mat))
+
+    # Assert the SVD has been calculated dynamically
+    assert all([hasattr(block, '_svd') for block in bd_matrix.blocks])
+
+    # Assert the log determinant exists
+    assert np.isclose(bd_matrix.log_determinant, np.linalg.slogdet(bd_matrix.data)[1])
