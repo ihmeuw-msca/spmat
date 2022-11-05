010e6ead08a1ce8e4f86628bd178b0f3cc6ce025
zhengp0 <zhengp@uw.edu>
2022-11-04 17:42:35 -0700

    add included file

--- setup.py
+++ setup.py
@@ -3,16 +3,16 @@ from pathlib import Path
 
 import numpy
 from Cython.Build import cythonize
-from setuptools import find_packages, setup
+from setuptools import Extension, find_packages, setup
 
 if __name__ == "__main__":
     base_dir = Path(__file__).parent
-    src_dir = base_dir/"src"/"spmat"
+    src_dir = base_dir / "src" / "spmat"
 
     sys.path.insert(0, src_dir.as_posix())
     import __about__ as about
 
-    with (base_dir/"README.rst").open() as f:
+    with (base_dir / "README.rst").open() as f:
         long_description = f.read()
 
     install_requirements = [
@@ -50,7 +50,7 @@ if __name__ == "__main__":
             "test": test_requirements,
             "dev": doc_requirements + test_requirements
         },
-        ext_modules=cythonize([str(src_dir / "linalg.pyx")]),
+        ext_modules=cythonize("src/spmat/linalg.pyx"),
         include_dirs=[numpy.get_include()],
         zip_safe=False,
     )
