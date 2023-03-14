9c9d547bb174386de812f72617d096bcdcb752ff
zhengp0 <zhengp@uw.edu>
2023-03-14 12:43:12 -0700

    add diag function to all matrices

--- tests/test_bdlmat.py
+++ tests/test_bdlmat.py
@@ -1,10 +1,10 @@
 """
 Test BDLMat
 """
-import pytest
 import numpy as np
-from spmat import BDLMat
+import pytest
 
+from spmat import BDLMat
 
 # pylint: disable=redefined-outer-name
 
@@ -15,9 +15,9 @@ NUM_BLOCKS = 3
 
 @pytest.fixture
 def bdlmat():
-    diag = np.random.rand(SHAPE[0]*NUM_BLOCKS) + 0.1
-    lmat = np.random.randn(SHAPE[0]*NUM_BLOCKS, SHAPE[1])
-    dsizes = [SHAPE[0]]*NUM_BLOCKS
+    diag = np.random.rand(SHAPE[0] * NUM_BLOCKS) + 0.1
+    lmat = np.random.randn(SHAPE[0] * NUM_BLOCKS, SHAPE[1])
+    dsizes = [SHAPE[0]] * NUM_BLOCKS
     return BDLMat(diag, lmat, dsizes)
 
 
@@ -27,18 +27,26 @@ def test_bdlmat(bdlmat):
     assert np.allclose(my_result, tr_result)
 
 
-@pytest.mark.parametrize("array", [np.random.randn(SHAPE[0]*NUM_BLOCKS),
-                                   np.random.randn(SHAPE[0]*NUM_BLOCKS,
-                                                   SHAPE[1])])
+@pytest.mark.parametrize(
+    "array",
+    [
+        np.random.randn(SHAPE[0] * NUM_BLOCKS),
+        np.random.randn(SHAPE[0] * NUM_BLOCKS, SHAPE[1]),
+    ],
+)
 def test_dot(bdlmat, array):
     my_result = bdlmat.dot(array)
     tr_result = bdlmat.mat.dot(array)
     assert np.allclose(my_result, tr_result)
 
 
-@pytest.mark.parametrize("array", [np.random.randn(SHAPE[0]*NUM_BLOCKS),
-                                   np.random.randn(SHAPE[0]*NUM_BLOCKS,
-                                                   SHAPE[1])])
+@pytest.mark.parametrize(
+    "array",
+    [
+        np.random.randn(SHAPE[0] * NUM_BLOCKS),
+        np.random.randn(SHAPE[0] * NUM_BLOCKS, SHAPE[1]),
+    ],
+)
 def test_invdot(bdlmat, array):
     my_result = bdlmat.invdot(array)
     tr_result = np.linalg.solve(bdlmat.mat, array)
@@ -49,3 +57,9 @@ def test_logdet(bdlmat):
     my_result = bdlmat.logdet()
     tr_result = np.linalg.slogdet(bdlmat.mat)[1]
     assert np.isclose(my_result, tr_result)
+
+
+def test_diag(bdlmat):
+    my_result = bdlmat.diag()
+    tr_result = np.diag(bdlmat.mat)
+    assert np.allclose(my_result, tr_result)
