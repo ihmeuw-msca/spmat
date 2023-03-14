5e3bd289bed71d5a30fcbbf6475f26524641ad9d
zhengp0 <zhengp@uw.edu>
2023-03-14 13:45:34 -0700

    advance version to 0.0.8

--- src/spmat/__about__.py
+++ src/spmat/__about__.py
@@ -1,13 +1,19 @@
 __all__ = [
-    "__title__", "__summary__", "__uri__", "__version__", "__author__",
-    "__email__", "__license__", "__copyright__",
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
 ]
 
 __title__ = "spmat"
 __summary__ = "spmat: A collection of tools for special matrices"
 __uri__ = "https://github.com/zhengp0/spmat"
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 
 __author__ = "Peng Zheng"
 __email__ = "zhengp@uw.edu"
