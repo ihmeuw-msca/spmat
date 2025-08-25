a36f6fcb1abbcd4f15531a1abeedae7cb3d8e299
saal <saal@uw.edu>
2025-08-25 14:38:56 -0700

    revert: old setup and pyrpoject setup

--- setup.py
+++ setup.py
@@ -12,12 +12,38 @@ if __name__ == "__main__":
     sys.path.insert(0, src_dir.as_posix())
     import __about__ as about
 
+    with (base_dir / "README.rst").open() as f:
+        long_description = f.read()
 
+    install_requirements = ["numpy", "scipy"]
+
+    test_requirements = [
+        "pytest",
+        "pytest-mock",
+    ]
+
+    doc_requirements = []
 
     setup(
+        name=about.__title__,
+        version=about.__version__,
+        description=about.__summary__,
+        long_description=long_description,
+        license=about.__license__,
+        url=about.__uri__,
+        author=about.__author__,
+        author_email=about.__email__,
+        python_requires=">=3.10",
         package_dir={"": "src"},
         packages=find_packages(where="src"),
         include_package_data=True,
+        install_requires=install_requirements,
+        tests_require=test_requirements,
+        extras_require={
+            "docs": doc_requirements,
+            "test": test_requirements,
+            "dev": doc_requirements + test_requirements,
+        },
         ext_modules=cythonize("src/spmat/linalg.pyx"),
         include_dirs=[numpy.get_include()],
         zip_safe=False,
