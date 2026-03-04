57a261a4c9d3110bfec965e42a2246140ef47878
zhengp <zhengp@uw.edu>
2026-03-03 20:47:58 -0800

    Switch to use extention rather than cythonize

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import numpy
-from Cython.Build import cythonize
-from setuptools import setup
+from setuptools import Extension, setup
 
 setup(
-    ext_modules=cythonize("src/spmat/linalg.pyx"),
+    ext_modules=[Extension("spmat.linalg", ["src/spmat/linalg.pyx"])],
     include_dirs=[numpy.get_include()],
 )
